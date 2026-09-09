9373a59854f67e9ce5f80a7575b3943cb353c1b3
zhuobinggang <zhuobinggang@gmail.com>
2026-09-09 11:28:49 +0900

    cool

---
+++ test.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+target_columns = [
+    'Sentence',
+    'Writer_Joy'
+]
+
+df = pd.read_csv(
+    './wrime/wrime-ver2.tsv',
+    sep='\t',
+    nrows=20000, 
+    usecols=target_columns
+)
+
+sentences = []
+labels = []
+for index, row in df.iterrows():
+    sentences.append(row['Sentence'])
+    labels.append(1 if row['Writer_Sadness'] > 0 else 0)
